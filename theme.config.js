8966dfd795d780f474a1e130c12c43df78fa4f1b
matt <mattsnaccc@gmail.com>
2021-07-08 22:56:08 -0700

    refactor: theme.config.js include presence

--- theme.config.js
+++ theme.config.js
@@ -1,59 +1,33 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/punctuations/presence',
+  titleSuffix: ' – presence',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">presence</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        A simple and elegant solution to images for socials media!
       </span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#2f3136" />
+      <meta name="theme-color" content="#2f3136" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="presence: A simple solution to images for socials media!" />
+      <meta name="og:description" content="presence: A simple solution to images for socials media!" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
+      <meta name="twitter:image" content="https://docs.presence.im/og.png" />
+      <meta name="twitter:site:domain" content="presence.im" />
+      <meta name="twitter:url" content="https://presence.im" />
+      <meta name="og:title" content="presence: A simple solution to images for socials media!" />
+      <meta name="og:image" content="https://docs.presence.im/og.png" />
+      <meta name="apple-mobile-web-app-title" content="presence" />
       <link
         rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/favicon.ico"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
     </>
   ),
   search: true,
@@ -61,5 +35,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © presence.</>,
 }
