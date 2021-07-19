960cf71ca79f92cd029748d75db2994af27cff33
matt <mattsnaccc@gmail.com>
2021-07-19 11:59:06 -0700

    chore: for -> of

--- theme.config.js
+++ theme.config.js
@@ -5,7 +5,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">presence</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        A simple and elegant solution to images for socials media!
+        A simple and elegant solution to images of your socials media!
       </span>
     </>
   ),
